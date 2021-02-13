414aa1dbd639e8e304e1c117fffd8fcc8ac6af1b
Ned Wilbur <Ned612@Gmail.com>
2021-02-13 00:40:07 -0500

    Added stopwatch to prevent multiple rapid triggers

--- USBKVM/Events.cs
+++ USBKVM/Events.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Management;
-using System.Text;
 
 namespace USBKVM
 {
     internal static class Events
     {
+        /// <summary>
+        /// A timer that starts when a event is triggered. Prevents event from firing monitor switch multiple times rapidly.
+        /// </summary>
+        private static Stopwatch LastEventStopwatch { get; } = new Stopwatch();
+        private const int MinimumTimeBetweenEvents = 3000;
+
         // More Info: https://stackoverflow.com/questions/620144/detecting-usb-drive-insertion-and-removal-using-windows-service-and-c-sharp
         internal static void Start()
         {
             Console.Write("Starting USBHub connect/disconnect event listeners... ");
 
-            // start USBHub watchers (insert & remove)
             WqlEventQuery insertQuery = new WqlEventQuery("SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'");
             ManagementEventWatcher insertWatcher = new ManagementEventWatcher(insertQuery);
             insertWatcher.EventArrived += DeviceInsertedEvent;
@@ -24,17 +28,24 @@ namespace USBKVM
             removeWatcher.EventArrived += DeviceRemovedEvent;
             removeWatcher.Start();
 
+            LastEventStopwatch.Start();
             Console.Write("Done!" + Environment.NewLine);
         }
 
         private static void DeviceInsertedEvent(object sender, EventArrivedEventArgs e)
         {
+            if (LastEventStopwatch.ElapsedMilliseconds <= MinimumTimeBetweenEvents) return;
+            LastEventStopwatch.Restart();
+
             Console.WriteLine("===> USBHub Connected");
             SwitchToPC(true);
         }
 
         private static void DeviceRemovedEvent(object sender, EventArrivedEventArgs e)
         {
+            if (LastEventStopwatch.ElapsedMilliseconds <= MinimumTimeBetweenEvents) return;
+            LastEventStopwatch.Restart();
+
             Console.WriteLine("=/=> USBHub Disconnected");
             SwitchToPC(false);
         }
